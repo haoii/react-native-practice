cc6da619875f37f6477e0923927c96858164b784
haogaofeng <gf_hao@126.com>
2019-01-10 03:09:50 -0800

    commit basic practice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './basic_practice/App7_buttons2';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);

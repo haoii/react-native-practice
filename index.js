bc0999749f3150de4c4e24ffcf998cf9b3a99bc8
haogaofeng <gf_hao@126.com>
2019-01-29 12:59:30 +0800

    init windows and android

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './src/react_navigation_practice/hello_react_navigation';
+import App from './src/basic_practice/App7_buttons2';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
